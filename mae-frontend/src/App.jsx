17da29a2d0d6ddeefd3f9caa9f34e7135197b8c3
Yuxuan Shi <90660901+ROLFFFX@users.noreply.github.com>
2024-08-02 04:29:17 +0800

    initial commit

---
+++ mae-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./util/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ mae-frontend/src/pages/chat/Chat.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function Chat() {
+  return (
+    <Box sx={{ height: "100%", width: "100%" }} border={1}>
+      Chat
+    </Box>
+  );
+}
